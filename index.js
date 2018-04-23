dcf26eaccc263d13a2c045d9ac63f44f53093a96
Will T <toherwils@gmail.com>
2018-04-22 23:57:05 -0500

    Initial date

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import { renderer_start } from './modules/renderer.js';
 
 renderer_start('clock');
 
+document.getElementById('date')
+    .appendChild(document.createElement('p'))
+    .appendChild(document.createTextNode(
+        (new Date()).toDateString()
+    ));
+
 console.log("Reached the end of index.js");
