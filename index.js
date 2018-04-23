80db63196943fe2782e266ad514379a5c4315417
Will T <toherwils@gmail.com>
2018-04-23 09:31:15 -0500

    Update date every minute

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ import { renderer_start } from './modules/renderer.js';
 
 renderer_start('clock');
 
-document.getElementById('date')
-    .appendChild(document.createElement('p'))
-    .appendChild(document.createTextNode(
-        (new Date()).toDateString()
-    ));
+function updateDate() {
+    document.getElementById('date')
+        .appendChild(document.createElement('p'))
+        .appendChild(document.createTextNode(
+            (new Date()).toDateString()
+        ));
+}
+
+updateDate();
+// Repeat every minute
+window.setInterval(updateDate, 60000);
+
+
 
 console.log("Reached the end of index.js");
